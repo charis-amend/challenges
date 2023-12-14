a2de0412fb7a742a702d5928a74898efa2a5a3c1
Charis Amend <charis.amend@googlemail.com>
2023-12-14 09:43:40 +0100

    ölakjsdfö

---
+++ javascript-challenges/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,38 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const titlePost = "This is the social media post title";
+const textPost =
+  "here is a long text for the social media post. the post is about variables and numbers in the language javascript. you can seee thee text of the post is even going on and on.";
+let numberLikes = 34;
+const userNameCreatedPost = "Santa Claus";
+let isReported = true;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log("shows titlePost:", titlePost);
+console.log("shows textpost:", textPost);
+console.log("shows numberoflikes", numberLikes);
+console.log("shows username:", userNameCreatedPost);
+console.log("shows if post isReported", isReported);
+
+// increasing likes by one +1
+
+numberLikes += 1;
+console.log("show numberofLikes with one added", numberLikes);
+
+// --^-- write your code here --^--
