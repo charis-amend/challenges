dd3f681a508da51fc1861969d761d8ffdb09b869
Charis Amend <charis.amend@googlemail.com>
2023-12-19 17:07:07 +0100

    finsihed challenge

--- javascript-challenges/js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ javascript-challenges/js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -4,4 +4,22 @@ function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe");
 }
 
+
 // The exercise starts here!
+
+// this function-call returns a welcome message console log
+// this function-call calls the userName parameter
+function showWelcomeMessage(userName) {
+  console.log("Welcome " + userName + "! You are logged in now")
+}
+
+// the named callback-function:
+handleUserLogin(showWelcomeMessage)
+
+// the anonymous callback-function:
+// writing inside the  function-call!!
+
+handleUserLogin((userName) => {
+  console.log(`Welcome ${userName}! You are logged in now`)
+})
+
