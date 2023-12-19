a725a149347073eb7e871655e16efb36f5c162c3
Charis Amend <charis.amend@googlemail.com>
2023-12-19 16:01:21 +0100

    finished callback named and anonymous callbacks

--- javascript-challenges/js-callback-functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ javascript-challenges/js-callback-functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -8,6 +8,33 @@ function handleUserLogin(onSuccess) {
 // Call handleUserLogin below!
 
 // named function:
+// creating a function that should show the welcome message:
 function showWelcomeMessage() {
+  // this is the output of the function:
   console.log("Welcome! You are logged in now.")
 }
+// we tell the handleUserLogin function at the top to take the output of the other 
+// showWelcomeMessage function for its own output
+handleUserLogin(showWelcomeMessage)
+// handdleUserLogin is now calling the showWelcomeMessage function 
+// the argument of handleUserLogin is onSucess
+// so it should log the welcome Message onSucess! 
+
+
+// anonymous function: 
+// Call "handleUserLogin" again but this time ->
+// write an anonymous function inside the function call
+// dont have to name the function! 
+
+// callingg handleuserlogin function 
+handleUserLogin(
+  // not putt onSucess before!!! 
+  // the argument is the anonymous function!! 
+  // making an anonymous function -- there are only () no name!
+  () => {
+    // then i say what this anaonymous function should do 
+    console.log("Welcome! You are logged in now.")
+  }
+)
+
+
