e1dfb3794fec7d4f4d3cf29bfa6374a010b8d120
Charis Amend <charis.amend@googlemail.com>
2023-12-20 17:01:32 +0100

    doing everything back to normal

--- npm-linting-challenges/web-linting-basics-template/index.js
+++ npm-linting-challenges/web-linting-basics-template/index.js
@@ -2,13 +2,14 @@ function toggleButton(cards) {
   const card = document.querySelectorAll('[data-js="card"]');
   const buttons = card.querySelectorAll('[data-js="buttons"]');
   cards.forEach(card => {
-    const button = card.querySelector('[data-js="button"]');
-    const link = cards.querySelector('[data-js="link"]');
-    const arrow = card.querySelector('[data-js="arrow"]');
-    buttons.addEventListener('click', () => {
-      link.classList.toggle('hide');
-      arrow.classList.toggle('rotate');
-    });
-  });
+    const button = card.querySelector('[data-js="button"]')
+    const link = cards.querySelector('[data-js="link"]')
+    const arrow = card.querySelector('[data-js="arrow"]')
+    button.addEventListener('click', () => {
+      link.classList.toggle("hide")
+      arrow.classList.toggle("rotate")
+
+    })
+  })
 }
-console.log("another change...")
+
