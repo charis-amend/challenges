92dd374c33b5d8e357218f2df4813a5c4c3db427
Charis Amend <charis.amend@googlemail.com>
2024-01-03 17:27:01 +0100

    finsihed challenge

--- javascript-challenges/js-basics_dark-mode/js/index.js
+++ javascript-challenges/js-basics_dark-mode/js/index.js
@@ -1,3 +1,23 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+bodyElement.classList.add("dark");
+bodyElement.classList.remove("dark");
+
+// adding black backgroundcolor
+darkModeButton.addEventListener("click", () => {
+    body.classList.add("dark");
+});
+// adding light backgrounddcolor
+lightModeButton.addEventListener("click", () => {
+    body.classList.remove("dark");
+});
+// making the button switch between the options
+toggleButton.addEventListener("click", () => {
+    bodyElement.classList.toggle("dark");
+})
