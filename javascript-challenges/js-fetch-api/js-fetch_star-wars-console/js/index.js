a1851d54f423df03b5885feca9334e61383ffed5
Charis Amend <charis.amend@googlemail.com>
2024-01-08 16:01:23 +0100

    finished first star wars challenge

--- javascript-challenges/js-fetch-api/js-fetch_star-wars-console/js/index.js
+++ javascript-challenges/js-fetch-api/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,29 @@
-console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+    try {
+        const response = await fetch(url);
+        console.log(response);
+
+        if (response.ok) {
+            const allData = await response.json();
+            console.log('entire Data object: ', allData)
+
+            console.log("different values/ results:", allData.results)
+
+            const results = allData.results
+            const r2d2 = results.find((character) =>
+                character.name.toLowerCase() === "r2-d2")
+            console.log("info about r2d2:", r2d2)
+
+            const eyecolor = r2d2.eye_color
+            console.log("eye color R2D2:", eyecolor)
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 fetchData();
