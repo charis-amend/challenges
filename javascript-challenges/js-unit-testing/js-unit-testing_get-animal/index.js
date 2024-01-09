1fba62e565ebbcc8b4f729ea8a28c5cfad8acdb1
Charis Amend <charis.amend@googlemail.com>
2024-01-09 15:32:59 +0100

    finished testing unit animal challenge

--- javascript-challenges/js-unit-testing/js-unit-testing_get-animal/index.js
+++ javascript-challenges/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,12 @@
-export function getAnimal() {}
+export function getAnimal(animalParameter) {
+
+    if (animalParameter === "cats") {
+        return 'I totally love cats!'
+    }
+    // any word entered in the animalParameter will be used
+    else if (typeof animalParameter === "string") {
+        return `I like ${[animalParameter]}!`
+    }
+    // if no arguments
+    else { return 'I do not like animals at all!' }
+}
