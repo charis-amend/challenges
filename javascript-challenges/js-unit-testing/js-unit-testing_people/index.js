ca0a3d49d2133882e744b4d0beaf21b8576bddec
Charis Amend <charis.amend@googlemail.com>
2024-01-09 17:37:33 +0100

    updatedd challenge, not finsihed yeet

--- javascript-challenges/js-unit-testing/js-unit-testing_people/index.js
+++ javascript-challenges/js-unit-testing/js-unit-testing_people/index.js
@@ -2,18 +2,29 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((fullname) => {
+    `${fullname.firstName} ${fullname.lastName}`
+  });
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) { }
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => person.age === age)
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleNamesOlderThan(people, age) {
+  const oldPeople = people.filter((person) => person.age > age);
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleByLastName(people, lastName) { }
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) { }
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) { }
 
-export function getFullNamesSortedByAge(people) {}
+export function getFullNamesSortedByAge(people) {
+  const sortedPeople = people.sort((a, b) => a.age - b.age);
+  return getFullNames(sortedPeople);
+}
--- javascript-challenges/js-unit-testing/js-unit-testing_people/index.test.js
+++ javascript-challenges/js-unit-testing/js-unit-testing_people/index.test.js
@@ -48,22 +48,31 @@ test("getFirstNames() returns an array with the first names", () => {
   expect(result).toEqual(["Jane", "John", "Max", "Erika", "Luke"]);
 });
 
-test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe"', () => {});
+test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe"', () => {
+  const result = getFullNames(people)
+  expect(result).toEqual(["Jane Doe", "John Doe", "Max Mustermann", "Erika Musterfrau", "Luke Skywalker"]);
+});
 
-test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {});
+test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {
+  const result = getNameAndAge(people)
+  expect(result).toEqual(["Doe (23)", "Doe (23)", "Mustermann (23)", "Mustermann (23)", "Skywalker (23)"])
+});
 
-test("getPeopleByAge() returns an array of people of a certain age", () => {});
+test("getPeopleByAge() returns an array of people of a certain age", () => {
+  const result = getPeopleByAge(people, 18) // using an example age
+  expect(result).toEqual([])
+});
 
-test('getPeopleNamesOlderThan() returns an array of people full names older than a certain age, e.g. "Luke Skywalker"', () => {});
+test('getPeopleNamesOlderThan() returns an array of people full names older than a certain age, e.g. "Luke Skywalker"', () => { });
 
-test("getPeopleByLastName() returns an array of people of a certain lastName", () => {});
+test("getPeopleByLastName() returns an array of people of a certain lastName", () => { });
 
 // Hint: use the array method find() for the implementation
-test("findPersonById() returns the full object of a person", () => {});
+test("findPersonById() returns the full object of a person", () => { });
 
 // Hint: use the array method some() for the implementation
-test("isAnyoneOlderThan() returns true if any person is older than the given age", () => {});
+test("isAnyoneOlderThan() returns true if any person is older than the given age", () => { });
 
-test("isAnyoneOlderThan() returns false if no person is older than the given age", () => {});
+test("isAnyoneOlderThan() returns false if no person is older than the given age", () => { });
 
-test("getFullNamesSortedByAge() returns an array of full names sorted by age", () => {});
+test("getFullNamesSortedByAge() returns an array of full names sorted by age", () => { });
