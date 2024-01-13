2d60c1325777269cf8fbaeab43b59a5e06a8bfa2
Charis Amend <charis.amend@googlemail.com>
2024-01-13 21:51:40 +0100

    added handle form submit challenge finished complete

--- javascript-challenges/js-forms/js-forms_handle-form-submit/js/index.js
+++ javascript-challenges/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formInputData = new FormData(event.target)
+    const allInputs = Object.fromEntries(formInputData)
+    const firstName = form.firstName.value;
+    console.log("first name:", firstName, allInputs);
+
+    //calc badness
+    let a = parseInt(form.age.value)
+    let b = parseInt(form.badness.value)
+
+    const sumAgeBadness = a + b
+
+    console.log("the agebadness sum of ", firstName, "is", sumAgeBadness)
+    form.reset();
+})
