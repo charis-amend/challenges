a0cabe0b6c3f09e3af4744c3108657d317bb4c55
Charis Amend <charis.amend@googlemail.com>
2024-01-14 20:38:46 +0100

    sorted people complete

--- javascript-challenges/js-unit-testing/js-unit-testing_people/index.js
+++ javascript-challenges/js-unit-testing/js-unit-testing_people/index.js
@@ -1,16 +1,16 @@
 export function getFirstNames(people) {
-  return people.map((person) => person.firstName);
+  return people.map(person => person.firstName);
 }
 
 export function getFullNames(people) {
-  return people.map((person) => {
+  return people.map(person => {
     `${person.firstName} ${person.lastName}`
   });
 }
 
 export function getNameAndAge(people) {
-  return people.map((person) => {
-    `"${person.lastName} ${person.age}"`
+  return people.map(person => {
+    `"${person.lastName} (${person.age})"`
   })
 }
 
@@ -20,13 +20,26 @@ export function getPeopleByAge(people, age) {
 
 export function getPeopleNamesOlderThan(people, age) {
   const oldPeople = people.filter((person) => person.age > age);
+  return getFullNames(oldPeople)
 }
 
-export function getPeopleByLastName(people, lastName) { }
+export function getPeopleByLastName(people, lastName) {
+  return people.filter((person) => person.lastName === lastName)
+}
 
-export function findPersonById(people, id) { }
+export function findPersonById(people, id) {
+  return people.find((person) => person.id === id)
+}
 
-export function isAnyoneOlderThan(people, age) { }
+export function isAnyoneOlderThan(people, age) {
+  people.some((person) => {
+    if (person.age > age) {
+      return true;
+    } else (person.age < age){
+      return false;
+    }
+  })
+}
 
 export function getFullNamesSortedByAge(people) {
   const sortedPeople = people.sort((a, b) => a.age - b.age);
