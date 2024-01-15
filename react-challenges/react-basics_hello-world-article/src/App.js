76812d0a6848e5586249b740c95d674ee7f11945
Charis Amend <charis.amend@googlemail.com>
2024-01-15 11:03:26 +0100

    updated challenge

--- react-challenges/react-basics_hello-world-article/src/App.js
+++ react-challenges/react-basics_hello-world-article/src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <div> <HelloWorldArticle /></div>;
+}
+
+function HelloWorldArticle() {
+  <article>
+    <h1>Hello World! Article Header</h1>
+    <p>
+      Here is a text about the World.
+      <strong>This text is bold.</strong>
+      <br>Here is a gap of text in the article.</br>
+    </p>
+    {/* <p>
+      Here is the text describing the button
+      <button>here is a button</button>
+    </p> */}
+  </article>
+
 }
