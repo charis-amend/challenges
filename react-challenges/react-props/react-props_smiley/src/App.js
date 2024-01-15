4ce21951bf1c61be3231202cbcc99dd6ac1acc7d
Charis Amend <charis.amend@googlemail.com>
2024-01-15 16:45:19 +0100

    finished smiley challenge

--- react-challenges/react-props/react-props_smiley/src/App.js
+++ react-challenges/react-props/react-props_smiley/src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+    </div>
+  )
+}
+
+function Smiley({ isHappy }) {
+  function checkHappiness() {
+    if (isHappy === true) {
+      return "I am happy 🥰"
+    } else {
+      return "I am sad 😭"
+    }
+  }
+
+  return (
+    <div>
+      <h1>Hello, {checkHappiness()}</h1>
+    </div>
+  )
 }
