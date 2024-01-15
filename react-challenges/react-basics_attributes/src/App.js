a52ed7f20e533d37c3c8b29dc2d1a64ed48166d3
Charis Amend <charis.amend@googlemail.com>
2024-01-15 11:22:07 +0100

    finshedd attributes chalalenge

--- react-challenges/react-basics_attributes/src/App.js
+++ react-challenges/react-basics_attributes/src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <div>
+    <Article />
+  </div >;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Name of Fancy Article</h2>
+      <label id="label" htmlFor="id">Enter your text here:</label>
+      <input id="input" htmlFor="input"></input>
+      <a className="article__link" href="https://en.wikipedia.org/wiki/Taylor_Swift">
+        Who is Taylor Swift?
+      </a>
+    </article>
+  )
 }
