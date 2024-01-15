224de37afba7279876b17f83842e4622abdc3a59
Charis Amend <charis.amend@googlemail.com>
2024-01-15 11:59:21 +0100

    correctedd challenge

--- react-challenges/react-basics_attributes/src/App.js
+++ react-challenges/react-basics_attributes/src/App.js
@@ -2,20 +2,33 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>
-    <Article />
-  </div >;
+  return (
+    // this is an empty fragment <>
+    <>
+      <div>
+        <Article /> <ArticleTwo />
+      </div >
+    </>
+  )
 }
 
 function Article() {
   return (
     <article className="article">
       <h2 className="article__title">Name of Fancy Article</h2>
-      <label id="label" htmlFor="id">Enter your text here:</label>
-      <input id="input" htmlFor="input"></input>
+      <label htmlFor="input">Enter your text here:</label>
+      <input id="input" type="text" />
       <a className="article__link" href="https://en.wikipedia.org/wiki/Taylor_Swift">
         Who is Taylor Swift?
       </a>
+    </article>
+  )
+}
+function ArticleTwo() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Article 2</h2>
+      <p>text of article two.</p>
     </article>
   )
 }
