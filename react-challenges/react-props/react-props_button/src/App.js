215549af55079131998c6f1646e938a7a2516d8e
Charis Amend <charis.amend@googlemail.com>
2024-01-15 17:01:40 +0100

    finsished button challenge

--- react-challenges/react-props/react-props_button/src/App.js
+++ react-challenges/react-props/react-props_button/src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Button color="red" text="i am a disabled button" disabled={true} />
+      <Button color="blue" text="i am a button" disabled={false} />
+    </div>
+  )
+}
+
+function Button({ color, disabled, text }) {
+
+  return (
+    <div>
+      <button style={{ height: "100px", backgroundColor: color }} disabled={disabled}>
+        {text}</button>
+    </div >
+  )
 }
