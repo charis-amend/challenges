002d8e48d79a9f478dae86c414eec40403b4132e
Charis Amend <charis.amend@googlemail.com>
2024-01-15 11:09:09 +0100

    finished article world react challenge

--- react-challenges/react-basics_hello-world-article/src/App.js
+++ react-challenges/react-basics_hello-world-article/src/App.js
@@ -6,17 +6,22 @@ export default function App() {
 }
 
 function HelloWorldArticle() {
-  <article>
-    <h1>Hello World! Article Header</h1>
-    <p>
-      Here is a text about the World.
-      <strong>This text is bold.</strong>
-      <br>Here is a gap of text in the article.</br>
-    </p>
-    {/* <p>
+  return (
+    <article>
+      <h1>Hello World! Article Header</h1>
+      <p>
+        Here is a text about the World.
+        <strong>This text is bold.</strong>
+        <br></br>
+        Here is a gap
+        <br></br>
+        <br></br>
+        of text in the article.
+      </p>
+      {/* <p>
       Here is the text describing the button
       <button>here is a button</button>
     </p> */}
-  </article>
-
+    </article>
+  )
 }
