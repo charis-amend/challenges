7fdda21ec8398d6b0f7ff720b427059dd77de8de
Charis Amend <charis.amend@googlemail.com>
2024-01-16 11:03:22 +0100

    finsihed color box challenge

--- react-challenges/react-nesting/react-nesting_fragments/src/App.js
+++ react-challenges/react-nesting/react-nesting_fragments/src/App.js
@@ -5,9 +5,21 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <main className="flex-container">
+      <Boxes box={<Box />} />
+      {/* <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" /> */}
+
+    </main>
+  );
+}
+
+function Boxes({ children }) {
+  return (
+    <>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
-  );
+    </>
+  )
 }
