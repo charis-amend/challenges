94f8f4cfbff3b4428900ee020728cf19e9bfa295
Charis Amend <charis.amend@googlemail.com>
2024-01-17 21:28:59 +0100

    added styling for form and tabbar

--- react-challenges/react-journal-challenge/journal-challenge/src/components/Form-Section/Form__textarea.jsx
+++ react-challenges/react-journal-challenge/journal-challenge/src/components/Form-Section/Form__textarea.jsx
@@ -2,8 +2,9 @@
 export default function FormTextarea({ textareaInput, labelOfTextarea, nameOfTextarea }) {
     return (
         <>
-            <label className="form-textarea-input" htmlFor={textareaInput}>{labelOfTextarea}</label>
-            <textarea id={textareaInput} name={nameOfTextarea} rows="10" cols="20">
+            <label className="form-textarea-input-label" htmlFor={textareaInput}>{labelOfTextarea}</label>
+            <textarea className="form-textarea-inputfield"
+                id={textareaInput} name={nameOfTextarea} rows="10" cols="20">
             </textarea>
         </>
     )
