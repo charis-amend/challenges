478c8372273ce9024c90dd2d0bf6f2fe2dfafce0
Charis Amend <charis.amend@googlemail.com>
2024-01-19 12:33:49 +0100

    finsihed tags tags tags challenge react state3

--- react-challenges/react-state/react-state-3_tags/src/App.js
+++ react-challenges/react-state/react-state-3_tags/src/App.js
@@ -6,10 +6,20 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function AddTag(newTag) {
+    setTags([...tags, newTag])
+  }
+  function DeleteTag(tagToDelete) {
+
+    // var for remaining tags:
+    const filteredTags = tags.filter(tag => tag !== tagToDelete)
+    setTags(filteredTags);
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={AddTag} />
+      <List tags={tags} onDeleteTag={DeleteTag} />
     </main>
   );
 }
