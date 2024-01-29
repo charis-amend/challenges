da1a88c300fa9ddab2c3d8742bb38a5c68f8cac0
Charis Amend <charis.amend@googlemail.com>
2024-01-29 10:42:33 +0100

    update

--- react-challenges/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react-challenges/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,46 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+// mock the onChange callback:
+jest.mock("testing the onChange Callback mocking it first:", () => ({
+    onchange() {
+        return { pathname: jest.fn() }
+    }
+}))
 
-test("calls callback on every user input", async () => {});
+test("renders a label and an input with the correct attributes", async () => {
+    render(<Input
+        labelText={labelText}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        type="text"
+        value={value}
+        onChange={onChange}
+        required={required}
+    />)
+
+    const user = userEvent.setup();
+
+    const labelText = screen.getByLabelText("Name")
+    const placeholderInput = screen.toHaveAttribute("placeholder", { placeholder })
+    const nameInput = screen.toHaveAttribute("name", { name })
+    const valueInput = screen.toHaveAttribute("value", { value })
+    const requiredInput = screen.toHaveAttribute("required", { required })
+
+
+    await user.type(input, "some input text")
+
+    const resultOfInput = screen.getByRole("heading", {
+        name: "some input text", level: 2
+    })
+
+    expect(labelText).toBeInTheDocument();
+    expect(placeholderInput).toBeInTheDocument();
+    expect(nameInput).toBeInTheDocument();
+    expect(valueInput).toBeInTheDocument();
+    expect(requiredInput).toBeInTheDocument();
+
+});
+
+test("calls callback on every user input", async () => { });
--- react-challenges/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
+++ react-challenges/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Navigation from ".";
+import StyledLink from "."
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -7,4 +8,18 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders with two links 'Play' and 'History'", () => {});
+test("tests if navigation bar shows up:", () => {
+  render(<Navigation />);
+  const navigation = screen.getByRole("navigation")
+  expect(navigation).toBeInTheDocument();
+})
+
+test("renders with two links 'Play' and 'History'", () => {
+  render(<Navigation />)
+  const playButtonLink = screen.getByRole("link", { name: "Play" })
+  const historyButtonLink = screen.getByRole("link", { name: "History" })
+
+  expect(playButtonLink).toBeInTheDocument();
+  expect(historyButtonLink).toBeInTheDocument();
+});
+
