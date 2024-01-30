42ce747d56918012574d3dfa666f9fd0227b95d1
Charis Amend <charis.amend@googlemail.com>
2024-01-30 12:45:21 +0100

    finsihed iss tracker fetch reacht challenge

--- react-challenges/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-challenges/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,44 +1,60 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const { data, isLoading, error, mutate } = useSWR(URL, { refreshInterval: 5000 });
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  // transforming data to coords, so i can use it in return()
+  const coords = data;
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  if (isLoading) return <div className="spinner">Loading ... </div>
+  console.log("--- data ---: ", data,
+    "--- new var coords --- :", coords)
+  if (error) return <div className="error" />
+  console.log("data from useSWR():----", data)
+
+  // const [coords, setCoords] = useState({
+  //   longitude: 0,
+  //   latitude: 0,
+  // });
+
+  // async function getISSCoords() {
+  //   try {
+  //     const response = await fetch(URL);
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setCoords({ longitude: data.longitude, latitude: data.latitude });
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     getISSCoords();
+  //   }, 5000);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+      />
       <Controls
         longitude={coords.longitude}
         latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        onRefresh={() => mutate()}
       />
     </main>
   );
