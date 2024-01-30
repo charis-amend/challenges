6237e0e96fabb1160ba4b558c0bfdb731ef18920
Charis Amend <charis.amend@googlemail.com>
2024-01-30 11:37:24 +0100

    finished challenge

--- react-challenges/react-data-fetching_star-wars/pages/_app.js
+++ react-challenges/react-data-fetching_star-wars/pages/_app.js
@@ -1,10 +1,28 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+const fetcher = async (url) => {
+
+  const response = await fetch(url)
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    // Attach extra info to the error object.
+    error.info = await response.json()
+    error.status = response.status
+    throw error
+  } else {
+    return response.json()
+  }
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
