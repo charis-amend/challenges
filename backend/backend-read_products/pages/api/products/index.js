1b70ff20eb7aa82a6fa6b084d7a9aed3c86f3267
Charis Amend <charis.amend@googlemail.com>
2024-02-12 12:32:45 +0100

    update

--- backend/backend-read_products/pages/api/products/index.js
+++ backend/backend-read_products/pages/api/products/index.js
@@ -1,5 +1,18 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/models/connect";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+
+  await dbConnect();
+  if (request.method === "GET") {
+    try {
+      const products = await Product.find()
+      console.log(products)
+
+    } catch (error) {
+
+    }
+  }
+
   return response.status(200).json(products);
 }
